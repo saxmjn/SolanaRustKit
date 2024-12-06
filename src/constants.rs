b1a7ca86ef106c2fb84a74698aafbcfb3cff466f
Saksham Jain <cedaus97@gmail.com>
2024-12-06 15:31:53 +0000

    deleted constants.rs

--- src/constants.rs
+++
@@ -1,8 +0,0 @@
-pub const _SAMPLE_TXN: &str = "SAMPLE_TRANSACTION";
-pub const _SAMPLE_ADDRESS: &str = "2k5AXX4guW9XwRQ1AKCpAuUqgWDpQpwFfpVFh3hnm2Ha";
-pub const _BEFORE_TXN: Option<&str> = Some("SAMPLE_TRANSACTION_VALUE_BEFORE");
-pub const _AMOUNT: u64 = 100_000; // Amount in LAMPORTS
-pub const _TIP: u64 = 10_000; // Amount in LAMPORTS
-pub const _TO_ADDRESS: &str = "RECIPIENT_ADDRESS_HERE";
-pub const _ACC_PRIVATE_KEY_BYTES: &[u8] = b"YOUR_PRIVATE_KEY_HERE";
-pub const HELIUS_KEY: &str = "YOUR_API_KEY";
